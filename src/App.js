8aca563eb6282e3a688c367dd2679b1476db49f9
tanulo <tanulo@barossgabor.local>
2022-01-31 12:33:37 +0100

    frontend

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { Switch, Route, Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+import { Nav, Navbar, NavDropdown } from "react-bootstrap";
 
 import AuthService from "./services/auth.service";
 
@@ -52,6 +53,67 @@ class App extends Component {
 
     return (
       <div>
+
+
+<Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+      <Navbar.Brand href="#home">
+        
+        Dice Roller
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse id="responsive-navbar-nav">
+        <Nav className="mr-auto">
+          <Nav.Link href="#features">Features</Nav.Link>
+          <Nav.Link href="#pricing">Pricing</Nav.Link>
+
+          {showAdminBoard && (   
+
+          <NavDropdown title="Admin menü" id="collasible-nav-dropdown">
+            <NavDropdown.Item href="/AdatTorles">Típusok törlése</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.2">
+              Another action
+            </NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+            <NavDropdown.Divider />
+            <NavDropdown.Item href="#action/3.4">
+              Separated link
+            </NavDropdown.Item>
+          </NavDropdown>
+          
+          )}
+
+
+        </Nav>
+        <Nav>
+          <Nav.Link href="#deets">More deets</Nav.Link>
+          <Nav.Link eventKey={2} href="#memes">
+            Dank memes
+          </Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+
+
+
+
+
+
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+{/*Ez alatt van a régi menü*/}
+
         <nav className="navbar navbar-expand navbar-dark bg-dark">
           <Link to={"/"} className="navbar-brand">
             bezKoder
